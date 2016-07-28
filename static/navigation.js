46e81ae6cd992c9260e86f5fdba999e0f18ec22a
Alyssa Wang <alyssa.c.wang@gmail.com>
2016-07-27 21:21:53 -0400

    fixed menu

---
+++ static/navigation.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+$('#open').on('click', openNav);
+$('#close').on('click', closeNav);
